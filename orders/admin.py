e359cdccb6034562274ef2ffee7c9febaeb59cd9
awemulya <aweamulya@gmail.com>
2015-11-02 08:59:52 +0545

    admin site

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import OrderRow, Order, Item, Category
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderRow)
